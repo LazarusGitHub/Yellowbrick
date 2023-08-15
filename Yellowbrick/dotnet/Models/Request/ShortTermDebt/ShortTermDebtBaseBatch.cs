ff6806797c839c0974d126c8c7d9fdab212e2754
Lazarus <62400120+LazarusGitHub@users.noreply.github.com>
2023-08-15 15:48:35 -0700

    Update ShortTermDebtBaseBatch.cs

--- Yellowbrick/dotnet/Models/Request/ShortTermDebt/ShortTermDebtBaseBatch.cs
+++ Yellowbrick/dotnet/Models/Request/ShortTermDebt/ShortTermDebtBaseBatch.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Requests
+namespace Yellowbrick.Models.Requests
 {
     public class ShortTermDebtBaseBatch
     {
