7d8e130757546a6d38f0e6d46079363f58af3969
Lazarus <62400120+LazarusGitHub@users.noreply.github.com>
2023-08-15 15:46:46 -0700

    Update ShortTermDebt.cs

--- Yellowbrick/dotnet/Models/Domain/ShortTermDebts/ShortTermDebt.cs
+++ Yellowbrick/dotnet/Models/Domain/ShortTermDebts/ShortTermDebt.cs
@@ -1,4 +1,4 @@
-﻿using Sabio.Models.Domain.Lookups;
+﻿using Yellowbrick.Models.Domain.Lookups;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -6,11 +6,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain
+namespace Yellowbrick.Models.Domain
 {
     public class ShortTermDebt
     {
-        
         public int Id { get; set; }
         public ClientBase Client { get; set; }
         public LookUp LoanType { get; set; }
@@ -22,6 +21,5 @@ namespace Sabio.Models.Domain
         public int ModifiedBy { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
-
     }
 }
