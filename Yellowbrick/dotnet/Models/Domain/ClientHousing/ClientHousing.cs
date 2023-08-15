8b665904d42e337ca2f7de711a3b51e1fc4758e9
Lazarus <62400120+LazarusGitHub@users.noreply.github.com>
2023-08-15 15:42:53 -0700

    Update ClientHousing.cs

--- Yellowbrick/dotnet/Models/Domain/ClientHousing/ClientHousing.cs
+++ Yellowbrick/dotnet/Models/Domain/ClientHousing/ClientHousing.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sabio.Models.Domain
+namespace Yellowbrick.Models.Domain
 {
     public class ClientHousing
     {
